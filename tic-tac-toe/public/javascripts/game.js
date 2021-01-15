e76376e1b26bf2e29c7dd596338d0a3a9a89b05f
Ann-Marie Harper <annmarie.grace.harper@gmail.com>
2021-01-14 22:41:20 -0600

    Added the functionality for adding X and O, alternating players each time

--- tic-tac-toe/public/javascripts/game.js
+++ tic-tac-toe/public/javascripts/game.js
@@ -1,3 +1,17 @@
 window.addEventListener('DOMContentLoaded', ()=>{
 
 });
+
+let divSquares = document.getElementsByTagName('div');
+let currentPlayer = 0;
+
+document.addEventListener('click', (e) => {
+    let currentSquare = e.target;
+    if(e.target.className === 'square' && currentPlayer === 0) {
+        currentSquare.innerHTML = 'X';
+        currentPlayer ++;
+    }else {
+        currentSquare.innerHTML = 'O';
+        currentPlayer = 0;
+    }
+});
